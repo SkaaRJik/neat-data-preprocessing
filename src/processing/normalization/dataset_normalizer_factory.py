1bf6e4ffe238fe47ff5f7cb16409abaf3abe5825
fva <fva@sibdigital.net>
2021-04-19 16:54:09 +0800

    fix bug with normalization factory

--- src/processing/normalization/dataset_normalizer_factory.py
+++ src/processing/normalization/dataset_normalizer_factory.py
@@ -1,14 +1,14 @@
 from src.processing.normalization.dataset_normalizer import DatasetNormalizer
 
 from src.processing.normalization.dataset_min_max_normalizer import DatasetMinMaxNormalizer
-from src.processing.normalization.dataset_standard_normalizer import StandardScaler
+from src.processing.normalization.dataset_standard_normalizer import StandardScaler, DatasetStandardNormalizer
 
 
 class DatasetNormalizerFactory(object):
 
     def __init__(self):
         self._minMaxNormalizer: DatasetNormalizer = DatasetMinMaxNormalizer()
-        self._standardNormalizer: DatasetNormalizer = StandardScaler()
+        self._standardNormalizer: DatasetNormalizer = DatasetStandardNormalizer()
 
     def getNormalizer(self, name: str) -> DatasetNormalizer:
         switch = {
